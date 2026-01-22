ff1bc4f279b15f93715d65ca6c615b29ff839564
Satyam Sachan <satyamsachan04@gmail.com>
2026-01-22 09:42:09 +0530

    Create patterprinting.cpp

---
+++ Day-2/class/patterprinting.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int k=1;k<=i;k++){
+            cout<<i;
+        }
+        cout<<"\n";
+    }
+}
