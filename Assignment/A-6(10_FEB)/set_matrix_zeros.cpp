df4b8e2de86dc639625b782511cd9dc643f1004e
{satyam} <{satsachan524@gmail.com}>
2026-02-10 19:09:37 +0530

    Added assignment and class linked list problems

---
+++ Assignment/A-6(10_FEB)/set_matrix_zeros.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+using namespace std;    
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool frz = false;
+        bool fcz = false;
+
+        for (int j = 0; j < n; j++)
+            if (matrix[0][j] == 0)
+                frz = true;
+
+        for (int i = 0; i < m; i++)
+            if (matrix[i][0] == 0)
+                fcz = true;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        if (frz) {
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+
+        if (fcz) {
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
+};
